1076f845db5afeb1c3d008bef6f9ccab8fb7b6eb
prmoreira23 <prm.gredes@gmail.com>
2018-08-14 08:25:44 -0400

    Add :id to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :first_name, :last_name, :full_name, :email, :phone, :job_title, :industry
+  attributes :id, :first_name, :last_name, :full_name, :email, :phone, :job_title, :industry
 end
